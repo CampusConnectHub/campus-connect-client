1d65abd4b8c350ccdffb6f335869fae49093111b
RISHIT GHOSH <rishitghosh06@gmail.com>
2025-10-11 13:55:18 +0530

    FRONTEND - CampusConnect.java
    Contains all the Cover and Overview linker to start the client.
    
    Signed-of-by: RISHIT GHOSH @rajghosh06<rishitghosh06@gmail.com>

---
+++ frontend/src/main/CampusConnect.java
@@ -0,0 +1,12 @@
+import ui.LoginScreen;//Directory
+//Packages within JSL
+import java.awt.*;
+import java.awt.event.*;
+
+public class CampusConnect
+{
+    public static void main(String[] args)
+    {
+        new LoginScreen(); // Launch login screen
+    }
+}
